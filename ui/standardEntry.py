7268beca1e3843050384d063596c382d8f8a0a58
Gloomn <ithan0704@naver.com>
2022-12-18 12:32:50 +0900

    UI update

---
+++ ui/standardEntry.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+def stdEntry(x, y, width, borderWidth, borderBackground, foreGroundColor, backGroundColor, fontFamily, fontSize, text, textVariable, show):
+
+
+    standardEntry = Entry(width = width,
+                          fg=foreGroundColor,
+                          bg=backGroundColor,
+                          text=text,
+                          font=(fontFamily, fontSize),
+                          relief=GROOVE,
+                          textvariable=textVariable,
+                          borderwidth=0,
+                          highlightbackground=borderBackground,
+                          highlightthickness=borderWidth,
+                          show=show)
+
+    standardEntry.place(x=x, y=y)
